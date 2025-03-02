8fe7a6abf41e15b42d338a2bc13e0a2e12ed4392
Charlie Dougherty <charlieldougherty@gmail.com>
2025-03-02 11:16:15 +0100

    changes to config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,19 @@
 use dotenv::dotenv;
 use std::env;
 use crate::models::{WeatherData, WeatherError};
+use reqwest::Client;
 
 pub struct WeatherApi {
     api_key: String,
     base_url: String,
+    client: Client;
 }
 
 impl WeatherApi {
-    pub fn new() -> Result<WeatherApi, WeatherError> {
+    pub fn new() -> Self {
         dotenv().ok();
         let api_key = env::var("WEATHER_API_KEY").expect("WEATHER_API_KEY must be set");
         let base_url = env::var("WEATHER_API_URL").expect("WEATHER_API_URL must be set");
 
-        ok(WeatherApi { api_key, base_url })
+        Ok(WeatherApi { api_key, base_url })
 }
